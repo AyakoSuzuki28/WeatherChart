223794287d264d65a002a6ac2e90eaf895752370
Ayako Suzuki <148215170+AyakoSuzuki28@users.noreply.github.com>
2024-01-11 11:13:13 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const url = 'https://api.open-meteo.com/v1/forecast?latitude=43.0667&longitude=141.35&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo';
+
+fetch (url)
+.then(data => data.json())
+.then(json => drawChart(json))
+
+function drawChart(json){
+  const mydata = {
+    labels : json.daily.time,
+    datasets : [{
+      label: `最高気温`,
+      data: json.daily.temperature_2m_max,
+      borderColor:`rgb(192,75,75)`,
+    },{
+      label:`最低気温`,
+      data: json.daily.temperature_2m_min,
+      borderColor:`rgb(75,75,192)`,
+    }]
+  }
+
+new Chart(document.getElementById('stage'),{
+  type: 'line',
+  data: mydata,
+});
+}
